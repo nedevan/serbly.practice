1ae0375e5c19f2e7ced1ce3c73be3840b650f583
Алексей Оникиенко <prorokpro84@gmail.com>
2025-05-15 18:40:11 +0300

    Проверка дз

--- src/main/java/com/example/practice/entity/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.example.practice.entity;
-
-public enum RoleType {
-    ROLE_ADMIN,
-    ROLE_USER
-}
--- src/main/java/com/example/practice/exception/AlreadyExitsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.practice.exception;
-
-public class AlreadyExitsException extends RuntimeException {
-    public AlreadyExitsException(String message) {
-        super(message);
-    }
-}
